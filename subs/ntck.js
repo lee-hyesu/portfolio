b8456429084f52f44b27c867d8fd10fa1bf5b0d0
lee-hyesu <ragnarok94@naver.com>
2024-06-25 10:50:29 +0900

    프로젝트 상세 페이지 수정

--- subs/ntck.js
+++ subs/ntck.js
@@ -2,7 +2,9 @@ window.onscroll = function () {
   var scrollPosition = window.scrollY;
   if (scrollPosition >= 2100) {
     $('.go-to-top').css('opacity',"1")
+    $('.go-to-top').css('scale',"1")
   } else {
     $('.go-to-top').css('opacity',"0")
+    $('.go-to-top').css('scale',"0")
   }
 };
