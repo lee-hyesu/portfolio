31cbeda44e0d36ad9fe455520588a5291bc267a9
lee-hyesu <ragnarok94@naver.com>
2024-08-13 00:05:54 +0900

    라이엇 게임즈 수정 및 작업물 추가

--- works/riot-games/index.js
+++ works/riot-games/index.js
@@ -17,6 +17,34 @@ const swiper1 = new Swiper(".visual-swiper", {
   speed: 800,
 });
 
+// gsap.registerPlugin(ScrollTrigger);
+// ScrollTrigger.matchMedia({
+//   '(min-width:1025px)':function(){
+//       // 섹션2 아래서 올라오는거
+//       ScrollTrigger.create({
+//           trigger:'.s1',
+//           start:'top top',
+//           // end:'+=250%',
+//     end:'+=100%',
+//           pin:true,
+//           // animation:s1_tl01,
+//           // scrub:1,
+//       })
+
+//       gsap.timeline({
+//           scrollTrigger:{
+//               trigger:'.s1',
+//               // end:'100%',
+//               scrub:1,
+//           }
+//       })
+//       .to('.s2',{marginTop:'-100vh',})
+//   },
+//   '(max-width:1024px)':function(){
+
+//   }
+// })
+
 const swiper2 = new Swiper(".s2-swiper", {
   pagination: false,
   navigation: {
@@ -31,6 +59,20 @@ const swiper2 = new Swiper(".s2-swiper", {
   slideWidth: window.innerWidth / 5,
   // spaceBetween: 48,
   centeredSlides: true,
+  // breakpoints: {
+  //   0: {
+  //     slidesPerView: 1.5,
+  //     spaceBetween: 24,
+  //   },
+  //   768: {
+  //     slidesPerView: 3.2,
+  //     spaceBetween: 24,
+  //   },
+  //   1024: {
+  //     slidesPerView: 4.3,
+  //     spaceBetween: 48,
+  //   },
+  // },
 });
 
 // s4
